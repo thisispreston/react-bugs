9b2cd7955b5c3216ab8edd9bd2d427465639d196
Matias Perez Ferrero <matias@Matiass-MacBook-Pro.local>
2020-02-20 09:13:26 -0700

    fresh bugs

---
+++ bugs-2/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Item from './Item';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      groceryList: ['apples', 'bread', 'milk', 'cheese']
+    };
+  }
+
+  renber() {
+    const items = this.state.groceryList.map(item => {
+      return <Item key={item} item={item} />;
+    });
+
+    return (
+      <div className="">
+        <h1>Grocery List</h1>
+        {items}
+      </div>
+    );
+  }
+}
---
+++ bugs-2/src/Item.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Item() {
+  return (
+    <div className="">
+      <input type="checkbox" />
+      {this.props.item}
+    </div>
+  );
+}
